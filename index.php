768cb1ac29995d30c3240e685c9a6773dad497b1
Fahad <iqbal.fahad90@gmail.com>
2014-12-19 15:17:27 -0600

    Added Ignore File and made some changes to Index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    $sites = json_decode(file_get_contents('sites.json'), true);
-    $resouces = json_decode(file_get_contents('resources.json'), true);
+    include_once('config.php');
+    $sites = json_decode(file_get_contents(PROJECTS_JSON), true);
+    $resouces = json_decode(file_get_contents(RESOURCE_JSON), true);
 ?>
 
 <!DOCTYPE html>
@@ -17,24 +18,14 @@
         <div class="row">
             <div class="col-md-3">
                 <h2>Current Projects</h2>
-                <?php
-                    foreach($sites as $site => $location)
-                    {
-                        printf("<li><a href='http://%s'>%s</a></li>",$location,$site);
-                    }
-                ?>
+                <?php OutputLinks($sites); ?>
             </div>
             <div class="col-md-6">
                 <h2>Adding New Project</h2>
             </div>
             <div class="col-md-3">
                 <h2>Resources</h2>
-                <?php
-                    foreach($resouces as $site => $location)
-                    {
-                        printf("<li><a href='http://%s'>%s</a></li>",$location,$site);
-                    }
-                ?>
+                <?php OutputLinks($resouces); ?>
             </div>
         </div>
     </div>
